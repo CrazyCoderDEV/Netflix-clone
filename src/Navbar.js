21f10736f9e0a61feec75dcbed08df65d5b30af2
Aditya Bhattachrajee <aditya2009student@gmail.com>
2021-01-30 15:12:26 +0600

    Initial Commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,20 @@
-import React from "react";
 import "./Navbar.css";
+import React, { useState, useEffect } from "react";
 
 function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        handleShow(true)
+      }
+      else handleShow(false);
+    })
+  })
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
@@ -11,7 +22,7 @@ function Navbar() {
       />
       <img
         className="nav_avatar"
-        src="https://www.flaticon.com/svg/vstatic/svg/3237/3237472.svg?token=exp=1611817324~hmac=da4b72eb31c43e9b46ce96dc4be8e489"
+        src="https://www.flaticon.com/svg/vstatic/svg/727/727399.svg?token=exp=1611996397~hmac=976468c933f9590d4369b3e9c2c51f21"
         alt="Avatar"
       />
     </div>
